21d809d7d245afc1004deb25f9793da6a14f0f64
Volkan <Volkan@VLKN08PC01>
2021-05-31 15:08:24 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SearchStringInTextFile
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var found = new List<string>();
+            var firstTwoCharacters = new List<string>();
+
+            using (var file = new StreamReader("E:\\Test.txt"))
+            {
+                string line;
+                while ((line = file.ReadLine()) != null)
+                    if (line.Substring(0, 1).Contains("3"))
+                        found.Add(line);
+            }
+
+            foreach (var item in found) Console.WriteLine(item);
+
+            Console.ReadKey();
+
+            //foreach (var item in found)
+            //{
+            //    var firstTwoChar = item.Substring(0, 2);
+            //    firstTwoCharacters.Add(firstTwoChar);
+            //}
+
+            //var distinct = firstTwoCharacters.Distinct().ToList();
+
+            //foreach (var value in distinct) Console.WriteLine(value);
+            //Console.ReadKey();
+        }
+    }
+}
